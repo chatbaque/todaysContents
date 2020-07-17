49e3d252fb4df00ba96389b8ebf0f15522462c6b
yonnii <rkdtjdus753@gmail.com>
2020-07-18 03:16:11 +0900

    사용자의 상태에 맞게 컨텐츠를 추천하는 다이얼로그의 구조 구현

---
+++ chatbaqueBot/chatbaqueBot/chatbaqueBot/Dialogs/SuggestContentsDialog.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class SuggestContentsDialog : ComponentDialog
+    {
+        public SuggestContentsDialog()
+    : base(nameof(SuggestContentsDialog))
+        {
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+                {
+                    SuggestStepAsync,
+                    ConfirmStarStepAsync,
+                    StarStepAsync
+                }));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> SuggestStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userInfoList = stepContext.Options as Dictionary<string, object>;
+            foreach (KeyValuePair<string, object> item in userInfoList)
+            {
+                stepContext.Values[item.Key] = item.Value;
+            }
+
+            var msg = $"감정 상태인 {stepContext.Values["emotion"]}에 따른 추천 메세지,,," +
+                $"\n\n\n추천 데이터...\n\n\n";
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
+            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("서비스가 만족스러우셨나요? 별점은 큰 힘이 됩니다.") }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ConfirmStarStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            if ((bool)stepContext.Result)
+            {
+                return await stepContext.NextAsync(null, cancellationToken);
+            }
+            else
+            {
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text("사용해주셔서 감사합니다. 서비스를 다시 시작하길 원하시면 아무 글자를 입력해주세요."), cancellationToken);
+                return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+            }
+
+
+        }
+
+        private async Task<DialogTurnResult> StarStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("별점 감사합니다. 서비스를 다시 시작하길 원하시면 아무 글자를 입력해주세요."), cancellationToken);
+
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+
+
+
+    }
+}

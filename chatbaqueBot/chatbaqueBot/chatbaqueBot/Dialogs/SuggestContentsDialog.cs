0c53aa9efbc0d6fd335be021dadd0a72e23a4ca7
yonnii <rkdtjdus753@gmail.com>
2020-07-22 15:46:01 +0900

    가져온 contents 데이터를 카드로 만들어서 화면에 출력

--- chatbaqueBot/chatbaqueBot/chatbaqueBot/Dialogs/SuggestContentsDialog.cs
+++ chatbaqueBot/chatbaqueBot/chatbaqueBot/Dialogs/SuggestContentsDialog.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Bot.Builder;
+﻿using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Choices;
-using Microsoft.Bot.Connector;
 using Microsoft.Bot.Schema;
-using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 
 namespace Microsoft.BotBuilderSamples
@@ -45,7 +39,21 @@ namespace Microsoft.BotBuilderSamples
             var msg = $"감정 상태인 {emotion}에 따른 {recommand_msg },,," + //감정에 따른 추천 메세지 case문같은거로 넣기만 하면됨
                 "\n\n\n추천 데이터\n\n\n";
 
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
+            await stepContext.Context.SendActivityAsync(createContentsCard((string)emotion), cancellationToken);
+            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("서비스가 만족스러우셨나요? 별점은 큰 힘이 됩니다.") }, cancellationToken);
+        }
 
+        private async Task<DialogTurnResult> ShowContentCardsStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+
+
+            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("서비스가 만족스러우셨나요? 별점은 큰 힘이 됩니다.") }, cancellationToken);
+        }
+
+
+        private static IMessageActivity createContentsCard(string emotion)
+        {
             HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
             HtmlAgilityPack.HtmlDocument doc = web.Load("https://pedia.watcha.com/ko-KR/" + "search?query=" + emotion.ToString());
             string[] contents_str = new string[4];
@@ -65,7 +73,6 @@ namespace Microsoft.BotBuilderSamples
             string[] contents_list = new string[100];
             int cnt_str = 0;
             int book_start_idx = movie.Length + tv.Length - 2;
-
             foreach (var item in doc.DocumentNode.SelectNodes("//div[@class ='css-1t67tr2-Titles e32z22d4']"))
             {
                 contents_list[cnt_str] = item.InnerText;
@@ -74,36 +81,47 @@ namespace Microsoft.BotBuilderSamples
                 cnt_str++;
             }
 
-            //랜덤 뽑기
+            var attachments = new List<Attachment>();
+            var reply = MessageFactory.Attachment(attachments);
+            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            var msg = "";
             Random random_movie = new Random();
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 4; i++)
             {
                 int movie_idx = random_movie.Next(0, movie.Length - 1);
                 string[] movie_info = contents_list[movie_idx].Split(" ・ ");
-                msg += "영화제목: " + movie_info[0].Substring(0, movie_info[0].Length - 4) + "\n\n";
-                msg += "연도: " + movie_info[0].Substring(movie_info[0].Length - 4) + "\n\n";
-                msg += "국가: " + movie_info[1] + "\n\n\n\n";
+                string title = movie_info[0].Substring(0, movie_info[0].Length - 4);
+                string year = movie_info[0].Substring(movie_info[0].Length - 4);
+                string country = movie_info[1];
+                reply.Attachments.Add(GetHeroCard(title, year, country).ToAttachment());
 
             }
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 4; i++)
             {
                 int book_idx = random_movie.Next(book_start_idx, book_start_idx + book.Length + 1);
                 string[] book_info = contents_list[book_idx].Split(" ・ ");
-
-                msg += "책제목: " + book_info[0].Substring(0, book_info[0].Length - 4) + "\n\n";
-                msg += "연도: " + book_info[0].Substring(book_info[0].Length - 4) + "\n\n";
-                if (book_info.Length > 1)
-                    msg += "작가: " + book_info[1] + "\n";
-                msg += "\n\n\n\n";
+                string title = book_info[0].Substring(0, book_info[0].Length - 4);
+                string year = book_info[0].Substring(book_info[0].Length - 4);
+                string author = book_info[1];
+                reply.Attachments.Add(GetHeroCard(title, year, author).ToAttachment());
             }
-
-
-            await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
-            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("서비스가 만족스러우셨나요? 별점은 큰 힘이 됩니다.") }, cancellationToken);
+            return reply;
         }
 
+        public static HeroCard GetHeroCard(string title, string year, string country)
+        {
+            var heroCard = new HeroCard
+            {
+                Title = title,
+                Subtitle = year + " , " + country,
+                Images = new List<CardImage> { new CardImage("https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.microsoft.com/bot-framework") },
+            };
 
+            return heroCard;
+        }
 
+        
         private async Task<DialogTurnResult> ConfirmStarStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             if ((bool)stepContext.Result)
@@ -125,8 +143,6 @@ namespace Microsoft.BotBuilderSamples
 
             return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
         }
-
-
 
     }
 }

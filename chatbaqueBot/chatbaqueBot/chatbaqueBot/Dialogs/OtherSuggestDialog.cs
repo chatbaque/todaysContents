5e7170a6620c91049c37f9c41b6b9b6d981aa59a
yonnii <rkdtjdus753@gmail.com>
2020-07-23 03:48:17 +0900

    인기있는 컨텐츠 추천-카드로 컨텐츠 출력 기능 구현

--- chatbaqueBot/chatbaqueBot/chatbaqueBot/Dialogs/OtherSuggestDialog.cs
+++ chatbaqueBot/chatbaqueBot/chatbaqueBot/Dialogs/OtherSuggestDialog.cs
@@ -19,7 +19,8 @@ namespace Microsoft.BotBuilderSamples
             AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
                 {
                     SuggestThemeStepAsync,
-                    StarStepAsync
+                    ShowContentsStepAsync,
+                    EndStepAsync,
                 }));
             AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
             AddDialog(new TextPrompt(nameof(TextPrompt)));
@@ -27,17 +28,13 @@ namespace Microsoft.BotBuilderSamples
             InitialDialogId = nameof(WaterfallDialog);
         }
 
-        private static string[] choices = new[] { "박스오피스 인기 작품", "왓챠 인기 작품", "넷플릭스 인기 작품", "평균 별점이 높은 작품" };
-
-        private async Task<DialogTurnResult> StarStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        {
-            await stepContext.Context.SendActivityAsync(MessageFactory.Text("저희 서비스를 사용해주셔서 감사합니다. 아무 글자를 입력해주시면, 서비스를 다시 시작하실 수 있습니다."), cancellationToken);
-
-            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
-        }
+        private static string[] choices = new string[4];
+        private static Dictionary<string, List<Movie>> contents = new Dictionary<string, List<Movie>>();
+        private static HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
 
         private async Task<DialogTurnResult> SuggestThemeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
+            CollectContents();
             var heroCard = new HeroCard
             {
                 Title = "원하는 추천 테마를 선택하세요.",
@@ -53,11 +50,124 @@ namespace Microsoft.BotBuilderSamples
             }, cancellationToken);
         }
 
+        private static void CollectContents()
+        {
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://pedia.watcha.com/ko-KR/");
+            int i = 0;
+            foreach (var item in doc.DocumentNode.SelectNodes("//div[@class = 'css-gxko42-StyledHomeListContainer ebeya3l2']"))
+            {
+                foreach (var p_element in item.Descendants("p"))
+                {
+                    int divCnt = 0;
+                    switch (i)
+                    {
+                        case 0:
+                            divCnt = 4;
+                            break;
+                        case 1:
+                        case 2:
+                            divCnt = 3;
+                            break;
+                        default:
+                            divCnt = 2;
+                            break;
+                    }
+                    if (p_element.InnerText != "")
+                    {
+                        choices[i] = p_element.InnerText;
+                        contents[p_element.InnerText] = new List<Movie>();
+                        Console.WriteLine(p_element.InnerText);
+                    }
+                    int j = 0;
+                    foreach (var li_element in item.Descendants("li"))
+                    {
+                        var contentList = contents[p_element.InnerText];
+                        var a_element = li_element.LastChild;
+                        var title = a_element.Attributes["title"].Value;
+                        var url = a_element.Attributes["href"].Value;
+                        var div_elements = a_element.LastChild.Descendants("div");
+                        string info = "";
+                        foreach (var div_element in div_elements)
+                        {
+                            if(j%divCnt != 0)
+                            {
+                                info += div_element.InnerText + "\n\n";
+                            }
+
+                            if (j++ % divCnt == divCnt - 1)
+                            {
+                                contentList.Add(new Movie(
+                                    title = title,
+                                    url = url,
+                                    info = info
+                                    ));
+                            }
+                        }
+                    }
+                    i++;
+                }
+            }
+
+
+        }
+
         private async Task<DialogTurnResult> ShowContentsStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
+            var msg = "\n\n\n데이터를 수집하고 있습니다. 잠시만 기다려주세요. :)\n\n\n";
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
+
             var theme = choices[((FoundChoice)stepContext.Result).Index];
-            await stepContext.Context.SendActivityAsync(MessageFactory.Text((string)theme), cancellationToken);
+
+            var attachments = new List<Attachment>();
+            var reply = MessageFactory.Attachment(attachments);
+            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            Random random_movie = new Random();
+
+            foreach (Movie item in (List<Movie>)contents[theme])
+            {
+                string imgUrl = "https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg";
+
+                HtmlAgilityPack.HtmlDocument img_doc = web.Load(item.url);
+                var img_div = img_doc.DocumentNode.SelectSingleNode("//div[@class ='css-ds7f62-PosterWithRankingInfoBlock e1svyhwg10']");
+                if (img_div.HasAttributes)
+                    imgUrl = (img_div.ChildNodes[2].ChildNodes[1].Attributes["src"].Value);
+
+                reply.Attachments.Add(GetHeroCard(item.title, item.info, item.url, imgUrl).ToAttachment());
+            }
+            await stepContext.Context.SendActivityAsync(reply, cancellationToken);
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> EndStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("저희 서비스를 사용해주셔서 감사합니다. 아무 글자를 입력해주시면, 서비스를 다시 시작하실 수 있습니다."), cancellationToken);
+
             return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+
+
+        public static HeroCard GetHeroCard(string title, string year, string url, string img)
+        {
+            var heroCard = new HeroCard
+            {
+                Title = title,
+                Text = year,
+                Images = new List<CardImage> { new CardImage(img) },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "더보기", value: url) },
+            };
+
+            return heroCard;
+        }
+    }
+
+    public class Movie
+    {
+        public string title, url, info;
+        public Movie(string title, string url, string info = "")
+        {
+            this.title = title;
+            this.url = "https://pedia.watcha.com"+url;
+            this.info = info;
         }
     }
 }

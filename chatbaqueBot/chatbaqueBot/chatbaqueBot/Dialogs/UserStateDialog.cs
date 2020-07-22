1eb8fccb59c047cdcadbf4be65a7a5618aa37601
yonnii <rkdtjdus753@gmail.com>
2020-07-22 16:54:34 +0900

    사용자데이터 확인메세지 변경

--- chatbaqueBot/chatbaqueBot/chatbaqueBot/Dialogs/UserStateDialog.cs
+++ chatbaqueBot/chatbaqueBot/chatbaqueBot/Dialogs/UserStateDialog.cs
@@ -68,8 +68,12 @@ namespace Microsoft.BotBuilderSamples
             var choice = (FoundChoice)stepContext.Result;
             stepContext.Values["emotion"] = choice.Value;
 
-            string msg = $"{stepContext.Values["name"]}님은 현재 {stepContext.Values["emotion"]}을 느끼고 계시고, {stepContext.Values["age"]}살 입니다.\n\n\n분석중...\n\n";
+            string msg = $"{stepContext.Values["name"]}님은 현재 {stepContext.Values["age"]}살이며 {stepContext.Values["emotion"]}을 느끼고 계십니다.";
             await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
+
+            msg = $"분석중...";
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
+
             return await stepContext.ReplaceDialogAsync(nameof(SuggestContentsDialog), stepContext.Values, cancellationToken);
         }
 

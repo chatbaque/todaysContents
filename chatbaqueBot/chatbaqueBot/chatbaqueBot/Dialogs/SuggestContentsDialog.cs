eecfb91fda96b74b9489a636dd784d7375abd93d
yonnii <rkdtjdus753@gmail.com>
2020-07-23 17:43:49 +0900

    기분 맞춤 컨텐츠- 데이터 수집 방식 변경

--- chatbaqueBot/chatbaqueBot/chatbaqueBot/Dialogs/SuggestContentsDialog.cs
+++ chatbaqueBot/chatbaqueBot/chatbaqueBot/Dialogs/SuggestContentsDialog.cs
@@ -35,7 +35,7 @@ namespace Microsoft.BotBuilderSamples
 
             var emotion = stepContext.Values["emotion"];
             var recommand_msg = "";
-            var msg = $"감정 상태인 {emotion}에 따른 {recommand_msg }한 컨텐츠를 추천해드릴게요!" + //감정에 따른 추천 메세지 case문같은거로 넣기만 하면됨
+            var msg = $"감정 상태인 {emotion}에 어울리는 컨텐츠를 추천해드릴게요!" + //감정에 따른 추천 메세지 case문같은거로 넣기만 하면됨
                 "\n\n\n데이터를 수집하고 있습니다. 잠시만 기다려주세요. :)\n\n\n";
 
             await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
@@ -54,40 +54,59 @@ namespace Microsoft.BotBuilderSamples
             HtmlAgilityPack.HtmlDocument doc = web.Load("https://pedia.watcha.com/ko-KR/" + "search?query=" + emotion.ToString());
             string baseURL = "https://pedia.watcha.com";
             List<Contents> contentsList = new List<Contents>();
-            int i = 0;
-            foreach (HtmlNode item in doc.DocumentNode.SelectNodes("//a[@class ='css-oeirkd-LinkSelf e32z22d1']")) //이미지 파일 링크 가져오기
+
+
+            foreach (var item in doc.DocumentNode.SelectNodes("//li[@class = 'css-106b4k6-Self e3fgkal0']"))
             {
-                if (i++ > 26)
+                var a_element = item.FirstChild;
+                string url="";
+                if (a_element.Attributes["href"] != null)
                 {
-                    break;
+                    url = baseURL + a_element.Attributes["href"].Value;
                 }
-                var title = item.Attributes["title"].Value;
-                var year = item.InnerText.Replace(title, "");
-                var urll = baseURL + item.Attributes["href"].Value;
-                contentsList.Add(new Contents(title, year, urll));
-
-/*                HtmlAgilityPack.HtmlDocument img_doc = web.Load(baseURL + (urll));
-
-                var img_div = img_doc.DocumentNode.SelectSingleNode("//div[@class ='css-ds7f62-PosterWithRankingInfoBlock e1svyhwg10']");
-                if (img_div.HasAttributes)
-                    Console.WriteLine(img_div.ChildNodes[2].ChildNodes[1].Attributes["src"].Value);*/
-
+                string img = "";
+                foreach (var img_element in a_element.Descendants("img"))
+                {
+                    if (img_element.Attributes["src"] != null)
+                    {
+                        img = img_element.Attributes["src"].Value;
+                    }
+                }
+                string title = "", info = "", type = "";
+                var divParent = a_element.LastChild;
+                int cnt = 0;
+                foreach (var div in divParent.Descendants("div"))
+                {
+                    switch (cnt++)
+                    {
+                        case 0:
+                            title = div.InnerText;
+                            break;
+                        case 1:
+                            info = div.InnerText;
+                            break;
+                        default:
+                            type = div.InnerText;
+                            break;
+                    }
+                }
+                contentsList.Add(new Contents(
+                        title = title,
+                        info = info,
+                        url = url,
+                        type = type,
+                        img = img
+                    ));
             }
-
             var attachments = new List<Attachment>();
             var reply = MessageFactory.Attachment(attachments);
-            reply.Text = $"감정 상태인 {emotion}에 따른 {emotion},,,";
+            reply.Text = $"감정 상태인 {emotion}에 어울리는 컨텐츠입니다.";
             reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
             Random random_movie = new Random();
 
             foreach(Contents item in contentsList)
             {
-                HtmlAgilityPack.HtmlDocument img_doc = web.Load(item.url);
-                string imgUrl = "https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg";
-                var img_div = img_doc.DocumentNode.SelectSingleNode("//div[@class ='css-ds7f62-PosterWithRankingInfoBlock e1svyhwg10']");
-                if (img_div.HasAttributes)
-                    imgUrl = (img_div.ChildNodes[2].ChildNodes[1].Attributes["src"].Value);
-                reply.Attachments.Add(GetHeroCard(item.title, item.year, item.url, imgUrl).ToAttachment());
+                reply.Attachments.Add(GetHeroCard(item.title, $"{item.info}  {item.type}", item.url, item.img).ToAttachment());
             }
 
             return reply;
@@ -113,12 +132,14 @@ namespace Microsoft.BotBuilderSamples
 
     class Contents
     {
-        public string title, url, year;
-        public Contents(string title, string year, string url)
+        public string title, url, info, type, img;
+        public Contents(string title, string info, string url, string type, string img)
         {
             this.title = title;
-            this.year = year;
+            this.info = info;
             this.url = url;
+            this.type = type;
+            this.img = img;
         }
     }
 
